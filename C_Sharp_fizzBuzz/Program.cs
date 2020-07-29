d459910014c98f2e6ee7331368948eae500e176b
hagi <hahagigi77@gmail.com>
2020-07-30 02:26:09 +0900

    プロジェクト ファイルを追加します。

---
+++ C_Sharp_fizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace C_Sharp_fizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num01 = 1;
+            while (num01 <= 100)
+            {
+                if(num01 % 3 == 0 && num01 % 5 == 0 )
+                {
+                    Console.WriteLine("FizzBuzz");
+                } 
+                else if(num01 % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                } 
+                else if(num01 % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                } 
+                else
+                {
+                    Console.WriteLine(num01);
+                }
+                num01++;
+            }
+            
+        }
+    }
+}
